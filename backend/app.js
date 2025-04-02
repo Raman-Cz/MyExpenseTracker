3cc3cc2c8b3e3a847f65bcfb6b9a73303cba5eb4
Raman-Cz <ramankemwal@gmail.com>
2025-04-02 19:59:17 +0530

    made few changes

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const app = express();
 const { db } = require('./db/db.js');
 const {readdirSync} = require('fs');
 
+const path = require("path");
+
 
 require('dotenv').config();
 
@@ -13,6 +15,13 @@ const PORT = process.env.PORT;
 //middlewares
 app.use(express.json());
 app.use(cors());
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+      })
+}
 
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1',require('./routes/'+ route)))
